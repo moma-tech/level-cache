7c144f57a4d2c3772f6b080a3a0826c80abd7e05
Ivan <ivandjp@163.com>
2020-07-07 18:03:21 +0800

    redis cache

--- src/main/java/top/moma/levelcache/cache/redis/RedisCache.java
+++ src/main/java/top/moma/levelcache/cache/redis/RedisCache.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import top.moma.levelcache.setting.RedisCacheSetting;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * RedisCache
@@ -16,19 +17,57 @@ public class RedisCache extends AbstractValueAdaptingCache {
 
   private final String name;
 
-  private RedisTemplate<String, Object> redisTemplate;
+  /** 缓存过期时间 */
+  private long expiration;
+  /** 过期时间单位 */
+  private TimeUnit timeUnit;
+  /** 使用缓存名前缀 */
+  private boolean usePrefix;
+  /** 方法级硬刷新 */
+  private boolean hardRefresh;
+  /** 过期前自动刷新 */
+  private boolean autoRenew;
+  /** 自动刷新阈值 */
+  private long renewThreshold;
 
-  private RedisCacheSetting redisCacheSetting;
+  private RedisTemplate<String, Object> redisTemplate;
 
   protected RedisCache(
       String name,
       boolean allowNullValues,
-      RedisCacheSetting redisCacheSetting,
+      RedisCacheSetting setting,
+      RedisTemplate<String, Object> redisTemplate) {
+    this(
+        allowNullValues,
+        name,
+        setting.getExpiration(),
+        setting.getTimeUnit(),
+        setting.isUsePrefix(),
+        setting.isHardRefresh(),
+        setting.isAutoRenew(),
+        setting.getRenewThreshold(),
+        redisTemplate);
+  }
+
+  public RedisCache(
+      boolean allowNullValues,
+      String name,
+      long expiration,
+      TimeUnit timeUnit,
+      boolean usePrefix,
+      boolean hardRefresh,
+      boolean autoRenew,
+      long renewThreshold,
       RedisTemplate<String, Object> redisTemplate) {
     super(allowNullValues);
     this.name = name;
+    this.expiration = expiration;
+    this.timeUnit = timeUnit;
+    this.usePrefix = usePrefix;
+    this.hardRefresh = hardRefresh;
+    this.autoRenew = autoRenew;
+    this.renewThreshold = renewThreshold;
     this.redisTemplate = redisTemplate;
-    this.redisCacheSetting = redisCacheSetting;
   }
 
   @Override
@@ -43,7 +82,7 @@ public class RedisCache extends AbstractValueAdaptingCache {
 
   @Override
   public Object getNativeCache() {
-    return null;
+    return this.redisTemplate;
   }
 
   @Override
--- src/main/java/top/moma/levelcache/setting/RedisCacheSetting.java
+++ src/main/java/top/moma/levelcache/setting/RedisCacheSetting.java
@@ -1,5 +1,7 @@
 package top.moma.levelcache.setting;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * RedisCacheSetting
  *
@@ -7,5 +9,66 @@ package top.moma.levelcache.setting;
  * @version 1.0
  */
 public class RedisCacheSetting implements java.io.Serializable {
-	private static final long serialVersionUID = -3923012495086794506L;
+  private static final long serialVersionUID = -3923012495086794506L;
+
+  /** 缓存过期时间 */
+  private long expiration = 0;
+  /** 过期时间单位 */
+  private TimeUnit timeUnit = TimeUnit.MICROSECONDS;
+  /** 使用缓存名前缀 */
+  private boolean usePrefix = true;
+  /** 方法级硬刷新 */
+  private boolean hardRefresh = false;
+  /** 过期前自动刷新 */
+  private boolean autoRenew = false;
+  /** 自动刷新阈值 */
+  private long renewThreshold = 0;
+
+  public long getExpiration() {
+    return expiration;
+  }
+
+  public void setExpiration(long expiration) {
+    this.expiration = expiration;
+  }
+
+  public TimeUnit getTimeUnit() {
+    return timeUnit;
+  }
+
+  public void setTimeUnit(TimeUnit timeUnit) {
+    this.timeUnit = timeUnit;
+  }
+
+  public boolean isUsePrefix() {
+    return usePrefix;
+  }
+
+  public void setUsePrefix(boolean usePrefix) {
+    this.usePrefix = usePrefix;
+  }
+
+  public boolean isHardRefresh() {
+    return hardRefresh;
+  }
+
+  public void setHardRefresh(boolean hardRefresh) {
+    this.hardRefresh = hardRefresh;
+  }
+
+  public boolean isAutoRenew() {
+    return autoRenew;
+  }
+
+  public void setAutoRenew(boolean autoRenew) {
+    this.autoRenew = autoRenew;
+  }
+
+  public long getRenewThreshold() {
+    return renewThreshold;
+  }
+
+  public void setRenewThreshold(long renewThreshold) {
+    this.renewThreshold = renewThreshold;
+  }
 }
